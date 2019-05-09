29e1809eca4048876d7f5c7b5f02516c65c9ff15
tourneureric <tourneur.eric@gmail.com>
2019-05-09 18:30:16 +0200

    Add class JavaEmptyString + cleanup JavaLearn

--- src/JavaLearn.java
+++ src/JavaLearn.java
@@ -1,18 +1,33 @@
 
 public class JavaLearn {
+	
+	private static boolean onJavaTypes = false;
+	private static boolean onJavaDebug = false;
+	private static boolean onJavaTest = false;
+	private static boolean onJavaEmptyString = true;
+
 
 	public static void main(String[] args) {
 		
-		/*
-		JavaTypes javaTypes = new JavaTypes();
-		javaTypes.main();
+		if (onJavaTypes) {
+			JavaTypes javaTypes = new JavaTypes();
+			javaTypes.main();
+		}
 		
-		JavaDebug javaDebug = new JavaDebug();
-		javaDebug.main();
-		*/
+		if (onJavaDebug) {
+			JavaDebug javaDebug = new JavaDebug();
+			javaDebug.main();
+		}
 		
-		JavaTest javaTest = new JavaTest();
-		javaTest.main();
+		if (onJavaTest) {
+			JavaTest javaTest = new JavaTest();
+			javaTest.main();
+		}
+
+		if (onJavaEmptyString) {
+			JavaEmptyString javaEmptyString = new JavaEmptyString();
+			javaEmptyString.main();
+		}
 		
 	}
 
--- src/JavaTest.java
+++ src/JavaTest.java
@@ -8,18 +8,6 @@ public class JavaTest {
 	public void main() {
 		System.out.println("JavaTest::main()");
 
-		String s1 = new String("");
-		System.out.println("s1='" + s1 + "'");
-		
-		if (s1.isEmpty()) {
-			System.out.println("s1 is empty");
-		}
-		
-		if (s1 == null) {
-			System.out.println("s1 is null");
-		} else {
-			System.out.println("s1 is not null");
-		}
 	}
 	
 }
