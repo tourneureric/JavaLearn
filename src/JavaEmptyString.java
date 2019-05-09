51362b26ef03b08afb78757940aa9df2e0855b8b
tourneureric <tourneur.eric@gmail.com>
2019-05-10 00:03:47 +0200

    cleanup output

--- src/JavaEmptyString.java
+++ src/JavaEmptyString.java
@@ -7,68 +7,65 @@ public class JavaEmptyString {
 
 	public void main() {
 		System.out.println("JavaEmptyString::main()");
+		System.out.println("");
 
 		String s1 = new String("");
 		System.out.println("s1='" + s1 + "'");
-		
-		// This is Okay
-		if (s1.equals("")) {
-			System.out.println("s1.equals(\"\")");
-		} else {
-			System.out.println("! s1.equals(\"\")");
-		}
-		
-		// This is Okay
-		if (s1.isEmpty()) {
-			System.out.println("s1 is empty");
-		} else {
-			System.out.println("s1 is not empty");
-		}
-		
-		// This is Wrong
-		if (s1 == null) {
-			System.out.println("s1 is null");
-		} else {
-			System.out.println("s1 is not null");
-		}
-		
-		// This is Wrong
-		if (s1 == "") {
-			System.out.println("s1 == \"\"");
-		} else {
-			System.out.println("s1 != \"\"");
-		}
-		
 		String s2 = new String("ABC");
 		System.out.println("s2='" + s2 + "'");
+		System.out.println("");
 		
-		// This is Okay
+		System.out.println("This is Okay");
+		if (s1.equals("")) {
+			System.out.println("		(s1.equals(\"\")) true");
+		} else {
+			System.out.println("		(s1.equals(\"\") false");
+		}
 		if (s2.equals("")) {
-			System.out.println("s2.equals(\"\")");
+			System.out.println("	&&	(s2.equals(\"\")) true");
 		} else {
-			System.out.println("! s2.equals(\"\")");
+			System.out.println("	&&	(s2.equals(\"\")) false");
 		}
+		System.out.println("");
 		
-		// This is Okay
+		System.out.println("This is Okay");
+		if (s1.isEmpty()) {
+			System.out.println("		(s1.isEmpty()) true");
+		} else {
+			System.out.println("		(s1.isEmpty()) false");
+		}
 		if (s2.isEmpty()) {
-			System.out.println("s2 is empty");
+			System.out.println("	&&	(s2.isEmpty()) true");
 		} else {
-			System.out.println("s2 is not empty");
+			System.out.println("	&&	(s2.isEmpty()) false");
 		}
+		System.out.println("");
 		
-		// This is Wrong
+		System.out.println("This is Wrong");
+		if (s1 == null) {
+			System.out.println("		(s1 == null) true");
+		} else {
+			System.out.println("		(s1 == null) false");
+		}
 		if (s2 == null) {
-			System.out.println("s2 is null");
+			System.out.println("	&&	(s2 == null) true");
 		} else {
-			System.out.println("s2 is not null");
+			System.out.println("	&&	(s2 == null) false");
 		}
+		System.out.println("");
 		
-		// This is Wrong
-		if (s2 == "") {
-			System.out.println("s2 == \"\"");
+		System.out.println("This is Wrong");
+		if (s1 == "") {
+			System.out.println("		(s1 == \"\") true");
 		} else {
-			System.out.println("s2 != \"\"");
+			System.out.println("		(s1 == \"\") false");
 		}
+		if (s2 == "") {
+			System.out.println("	&&	(s2 == \"\") true");
+		} else {
+			System.out.println("	&&	(s2 == \"\") false");
+		}
+		System.out.println("");
 
 	}
 	
