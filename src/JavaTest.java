14ba0a1346432990b7a12175f85ab0ee6ba4227c
tourneureric <tourneur.eric@gmail.com>
2019-05-09 11:25:32 +0200

    Add JavaTest

---
+++ src/JavaTest.java
@@ -0,0 +1,14 @@
+
+public class JavaTest {
+
+	public JavaTest () {
+		System.out.println("JavaTest::JavaTest()");
+	}
+
+	public void main() {
+		System.out.println("JavaTest::main()");
+
+
+	}
+
+}
