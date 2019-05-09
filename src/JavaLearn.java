e879c15f9df7f1c7f6ff3790dd4d0abf3a9a09de
tourneureric <tourneur.eric@gmail.com>
2019-05-09 11:25:11 +0200

    cleanup

--- src/JavaLearn.java
+++ src/JavaLearn.java
@@ -3,14 +3,16 @@ public class JavaLearn {
 
 	public static void main(String[] args) {
 		
+		/*
 		JavaTypes javaTypes = new JavaTypes();
 		javaTypes.main();
 		
 		JavaDebug javaDebug = new JavaDebug();
 		javaDebug.main();
+		*/
 		
-		JavaTemplate javaTemplate = new JavaTemplate();
-		javaTemplate.main();
+		JavaTest javaTest = new JavaTest();
+		javaTest.main();
 		
 	}
 
