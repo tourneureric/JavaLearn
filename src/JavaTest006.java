1903a87588ce8c4240332de5e8aa94374299af63
tourneureric <tourneur.eric@gmail.com>
2019-05-21 17:30:56 +0200

    learn java

--- src/JavaTest006.java
+++ src/JavaTest006.java
@@ -14,32 +14,64 @@ public class JavaTest006 {
 		Log.log(Log.LOG_DEBUG, this.getClass().getName() + "::main() [begin]");
 		
 		if (JavaLearn.JL_RUN) {
-			
-			//Une variable vide
+			/*
 			String prenom;
-			//On initialise celle-ci à O pour oui
 			char reponse = 'O';
-			//Notre objet Scanner, n'oubliez pas l'import de java.util.Scanner !
 			Scanner sc = new Scanner(System.in);
-			//Tant que la réponse donnée est égale à oui…
 			while (reponse == 'O')
 			{
-			  //On affiche une instruction
 			  System.out.println("Donnez un prénom : ");
-			  //On récupère le prénom saisi
 			  prenom = sc.nextLine();
-			  //On affiche notre phrase avec le prénom
-			  System.out.println("Bonjour " +prenom+ ", comment vas-tu ?");
-			  //On demande si la personne veut faire un autre essai
-			  System.out.println("Voulez-vous réessayer ? (O/N)");
-			  //On récupère la réponse de l'utilisateur
-			  reponse = sc.nextLine().charAt(0);
+			  System.out.println("Bonjour " +prenom+ ", comment vas-tu ?");                        
+			  //Sans ça, nous n'entrerions pas dans la deuxième boucle
+			  reponse = ' ';
+			                        
+			  //Tant que la réponse n'est pas O ou N, on repose la question
+			  while(reponse != 'O' && reponse != 'N')
+			  {
+			    //On demande si la personne veut faire un autre essai
+			    System.out.println("Voulez-vous réessayer ? (O/N)");
+			    reponse = sc.nextLine().charAt(0);
+			  }
 			}
+			System.out.println("Au revoir…");
 			sc.close();
+			*/
+			
+			/*
+			String prenom = new String();
+			//Pas besoin d'initialiser : on entre au moins une fois dans la boucle !
+			char reponse = ' ';
+			 
+			Scanner sc = new Scanner(System.in);
+			 
+			do{
+			  System.out.println("Donnez un prénom : ");
+			  prenom = sc.nextLine();
+			  System.out.println("Bonjour " +prenom+ ", comment vas-tu ?");
+			       
+			  do{
+			    System.out.println("Voulez-vous réessayer ? (O/N)");
+			    reponse = sc.nextLine().charAt(0);
+			  }while(reponse != 'O' && reponse != 'N');
+			        
+			}while (reponse == 'O');
 			 
 			System.out.println("Au revoir…");
-			//Fin de la boucle
+			sc.close();
+			*/
 			
+			/*
+			for(int i = 1; i <= 10; i++)
+			{
+			  System.out.println("Voici la ligne "+i);
+			}
+			*/
+			
+			for(int i = 0, j = 2; (i < 20 && j < 30); i++, j+=2) {
+				  System.out.println("i = " + i + ", j = " + j);
+			}
+	
 		}
 		
 		Log.log(Log.LOG_DEBUG, this.getClass().getName() + "::main() [end]");
