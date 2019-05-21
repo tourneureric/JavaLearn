084b904ef4230fd29cec6009b581e558b49ab0c7
tourneureric <tourneur.eric@gmail.com>
2019-05-21 15:02:57 +0200

    T.R.U.E and F.A.L.S.E

--- src/JavaTest005.java
+++ src/JavaTest005.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class JavaTest005 {
 
@@ -10,6 +11,20 @@ public class JavaTest005 {
 	public void main() {
 		
 		System.out.println("JavaTest005::main() [begin]");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			System.out.println("Veuillez saisir un boolean (true|false) :");
+			boolean my_boolean = sc.nextBoolean();
+			System.out.println("my_boolean = " + my_boolean);
+			String choice = (my_boolean ? "[T.R.U.E]" : "[F.A.L.S.E]");
+			System.out.println("choice = " + choice);
+		} catch (Exception e) { System.out.println("ERROR " + e.getMessage()); }		
+
+		try {
+			sc.close();
+		} catch (Exception e) { System.out.println("ERROR"); }
 		
 		System.out.println("JavaTest005::main() [end]");
 
