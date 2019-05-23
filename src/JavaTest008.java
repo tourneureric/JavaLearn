f0ddb67d1ab89358c29943b131c8981881731883
tourneureric <tourneur.eric@gmail.com>
2019-05-23 14:52:42 +0200

    Add myTab001[][] & myTab002[][][]

--- src/JavaTest008.java
+++ src/JavaTest008.java
@@ -45,11 +45,40 @@ public class JavaTest008 {
 
 		Log.log(Log.LOG_DEBUG, this.getClass().getName() + "::doit() [begin]");
 
-		int myTab001[][] = { {0,2,4,6,8},{1,3,5,7,9} };
+		int myTab001[][] = { {0, 2, 4, 6, 8},{1, 3,5 ,7, 9} };
 
 		for (int i = 0; i < myTab001.length; i++) {
 			for (int j = 0; j < myTab001[i].length; j++) {
 				P.print("myTab001[" + i + "][" + j + "] = " + myTab001[i][j]);
+			}
+		}
+
+		String myTab002[][][] =
+			{
+					{
+						{"a", "b"},
+						{"c", "d", "e"}
+					},
+					{
+						{"f", "g"},
+						{"h", "i", "j", "k"}
+					},
+					{
+						{"l"},
+						{"m", "n", "o", "p", "q"},
+						{"r", "s"}
+					},
+					{
+						{"t", "u", "v", "w"},
+						{"x", "y", "z"}
+					}
+			};
+
+		for (int i = 0; i < myTab002.length; i++) {
+			for (int j = 0; j < myTab002[i].length; j++) {
+				for (int k = 0; k < myTab002[i][j].length; k++) {
+					P.print("myTab002[" + i + "][" + j + "][" + k + "] = " + myTab002[i][j][k]);
+				}
 			}
 		}
 
