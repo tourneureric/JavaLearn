298584e5659c6563703bba7479f118b29e4511e2
tourneureric <tourneur.eric@gmail.com>
2019-05-23 15:01:53 +0200

    Add launch of JavaTestTemplate

--- src/JavaLearn.java
+++ src/JavaLearn.java
@@ -3,6 +3,7 @@ public class JavaLearn {
 
 	public static final boolean JL_RUN = true;
 
+	private static boolean onJavaTestTemplate = false;
 	private static boolean onJavaTest001 = false;
 	private static boolean onJavaTest002 = false;
 	private static boolean onJavaTest003 = false;
@@ -16,6 +17,7 @@ public class JavaLearn {
 
 		Log.log(Log.LOG_DEBUG, "JavaLearn::main() [begin]");
 
+		if (onJavaTestTemplate) { JavaTestTemplate javaTest = new JavaTestTemplate(); javaTest.main(); }
 		if (onJavaTest001) { JavaTest001 javaTest = new JavaTest001(); javaTest.main(); }
 		if (onJavaTest002) { JavaTest002 javaTest = new JavaTest002(); javaTest.main(); }
 		if (onJavaTest003) { JavaTest003 javaTest = new JavaTest003(); javaTest.main(); }
