4511bcf8fb3a3040bd2c10c78d72b2b429453643
tourneureric <tourneur.eric@gmail.com>
2019-05-23 15:43:43 +0200

    Working on tab

--- src/JavaTest008.java
+++ src/JavaTest008.java
@@ -46,7 +46,7 @@ public class JavaTest008 {
 	private void doit() {
 
 		Log.log(Log.LOG_DEBUG, this.getClass().getName() + "::doit() [begin]");
-		
+
 		P.print("================================================================================");
 
 		int myTab001[][] = { {0, 2, 4, 6, 8},{1, 3,5 ,7, 9} };
@@ -80,10 +80,26 @@ public class JavaTest008 {
 					}
 			};
 
-		for (int i = 0; i < myTab002.length; i++) {
-			for (int j = 0; j < myTab002[i].length; j++) {
-				for (int k = 0; k < myTab002[i][j].length; k++) {
-					P.print("myTab002[" + i + "][" + j + "][" + k + "] = " + myTab002[i][j][k]);
+		int i3 = 0;
+		for (String myUtTab[][] : myTab002) {
+			i3++;
+			int j3 = 0;
+			for (String myUtUtTab[] : myUtTab) {
+				j3++;
+				int k3 = 0;
+				for (String myValue : myUtUtTab) {
+					k3++;
+					P.print("myTab002[" + i3 + "][" + j3 + "][" + k3 + "] = " + myValue);
+				}
+			}
+		}
+		
+		P.print("================================================================================");
+
+		for (int i5 = 0; i5 < myTab002.length; i5++) {
+			for (int j5 = 0; j5 < myTab002[i5].length; j5++) {
+				for (int k5 = 0; k5 < myTab002[i5][j5].length; k5++) {
+					P.print("myTab002[" + i5 + "][" + j5 + "][" + k5 + "] = " + myTab002[i5][j5][k5]);
 				}
 			}
 		}
@@ -105,6 +121,23 @@ public class JavaTest008 {
 			P.print(" La lettre " + carac + " se trouve bien dans le tableau (position " + i + ") !");
 		} else {
 			P.print(" La lettre " + carac + " ne se trouve pas dans le tableau !");
+		}
+
+		P.print("================================================================================");
+
+		String tab[][]={{"toto", "titi", "tutu", "tete", "tata"}, {"1", "2", "3", "4"}};
+		int i1 = 0, j1 = 0;
+
+		for(String sousTab[] : tab)
+		{
+			i1 = 0;
+			for(String str : sousTab)
+			{     
+				System.out.println("La valeur de la nouvelle boucle est  : " + str);
+				System.out.println("La valeur du tableau à l'indice ["+j1+"]["+i1+"] est : " + tab[j1][i1]);
+				i1++;
+			}
+			j1++;
 		}
 
 		P.print("================================================================================");
