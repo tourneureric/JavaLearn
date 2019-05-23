7e22eec3386c22bc5e3e0b0a493be92ecd1af3ac
tourneureric <tourneur.eric@gmail.com>
2019-05-23 13:44:36 +0200

    Add JavaTest008

---
+++ src/JavaTest008.java
@@ -0,0 +1,52 @@
+
+import java.util.Scanner;
+
+public class JavaTest008 {
+
+	public JavaTest008 () {
+
+		Log.log(Log.LOG_DEBUG, this.getClass().getName() + "::" + this.getClass().getName() + "()");
+
+	}
+
+	public void main() {
+
+		Log.log(Log.LOG_DEBUG, this.getClass().getName() + "::main() [begin]");
+
+		if (JavaLearn.JL_RUN) {
+
+			try {
+
+				String again = "n";
+
+				Scanner sc = new Scanner(System.in);
+
+				do {
+					
+					doit();
+
+					System.out.println("Voulez-vous continuer (o/n) ?");
+					again = sc.nextLine();
+					System.out.println("");
+
+				} while (again.equalsIgnoreCase("o"));
+
+				sc.close();
+
+			} catch (Exception e) { Log.error("in main()"); throw e; }
+
+			Log.log(Log.LOG_DEBUG, this.getClass().getName() + "::main() [end]");
+
+		}
+
+	}
+
+	private void doit() {
+
+		Log.log(Log.LOG_DEBUG, this.getClass().getName() + "::doit() [begin]");
+
+		Log.log(Log.LOG_DEBUG, this.getClass().getName() + "::doit() [end]");
+
+	}
+
+}
