293395cca2505d12fc9904394cef04b8eddec6eb
tourneureric <tourneur.eric@gmail.com>
2019-05-31 09:57:14 +0200

    OpenClassrooms / Java Orienté Objet / Gérez les héritages

--- src/com/tourneureric/javalearn002/JavaLearn.java
+++ src/com/tourneureric/javalearn002/JavaLearn.java
@@ -6,7 +6,8 @@ public class JavaLearn {
 	public static final boolean JL_RUN = true;
 
 	private static boolean onJavaTestTemplate = false;
-	private static boolean onJavaTest010 = true;
+	private static boolean onJavaTest010 = false;
+	private static boolean onJavaTest011 = true;
 
 	public static void main(String[] args) {
 
@@ -14,6 +15,7 @@ public class JavaLearn {
 
 		if (onJavaTestTemplate) { JavaTestTemplate javaTest = new JavaTestTemplate(); javaTest.main(); }
 		if (onJavaTest010) { JavaTest010 javaTest = new JavaTest010(); javaTest.main(); }
+		if (onJavaTest011) { JavaTest011 javaTest = new JavaTest011(); javaTest.main(); }
 
 		Log.log(Log.LOG_DEBUG, "JavaLearn::main() [end]");
 
