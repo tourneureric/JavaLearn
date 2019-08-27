ef745a016170d0f5e6cdb3e3262d5d9acf135769
tourneureric <tourneur.eric@gmail.com>
2019-08-27 12:48:05 +0200

    Modify class Guerrier add method soigner()

--- src/com/tourneureric/javalearn003/Guerrier.java
+++ src/com/tourneureric/javalearn003/Guerrier.java
@@ -4,4 +4,8 @@ public class Guerrier extends Personnage {
 	public void combattre() {
 		System.out.println("Fusil, pistolet, couteau ! Tout ce que tu veux !");
 	}
+	
+	public void soigner() {
+		System.out.println("Je prodigue les premiers soins.");
+	}
 }
