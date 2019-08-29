d44275f0afe19c215a2f11105ca66643b2b5a6b4
tourneureric <tourneur.eric@gmail.com>
2019-08-29 13:11:25 +0200

    Create package com.tourneureric.javalearn004

---
+++ src/com/tourneureric/javalearn004/Log.java
@@ -0,0 +1,35 @@
+package com.tourneureric.javalearn004;
+
+public class Log {
+
+	public static final int LOG_INFO = 1;
+	public static final int LOG_TRACE = 2;
+	public static final int LOG_DEBUG = 3;
+
+	private static final boolean LOG_ON = true;
+	private static final int LOG_LEVEL = LOG_DEBUG;
+
+	public static final void log(int pLevel, String pText) {
+		if (LOG_ON && pLevel <= LOG_LEVEL) {
+			switch (pLevel) {
+			case LOG_INFO:
+				System.out.println("INFO " + pText);
+				break;
+			case LOG_TRACE:
+				System.out.println("TRACE " + pText);
+				break;
+			case LOG_DEBUG:
+				System.out.println("DEBUG " + pText);
+				break;
+			default:
+				System.out.println(pText);
+				break;
+			}
+		}
+	}
+
+	public static final void error(String pText) {
+		System.out.println("ERROR " + pText);
+	}
+
+}
---
+++ src/com/tourneureric/javalearn004/P.java
@@ -0,0 +1,13 @@
+package com.tourneureric.javalearn004;
+
+public class P {
+
+	private static final boolean PRINT_ON = true;
+
+	public static final void print(String pText) {
+		if (PRINT_ON) {
+			System.out.println(pText);
+		}
+	}
+
+}
