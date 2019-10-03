5b45e7dc8c681d490e1198d07efec873959c1294
tourneureric <tourneur.eric@gmail.com>
2019-10-03 14:50:31 +0200

    Gérez les exceptions

--- src/com/tourneureric/javalearn005/JavaTest001.java
+++ src/com/tourneureric/javalearn005/JavaTest001.java
@@ -9,6 +9,14 @@ public class JavaTest001 {
 	private void doit() {
 
 		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [begin]");
+		
+		int j = 20, i = 0;
+		try {
+			System.out.println(j/i);
+		} catch (ArithmeticException e) {
+			System.out.println("Division par zéro ! [" + e.getMessage() + "]");
+		}
+		System.out.println("coucou toi !");
 
 		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [end]");
 
