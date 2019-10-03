afdf98949c0038634b325226e7ed277f4856ee8a
tourneureric <tourneur.eric@gmail.com>
2019-10-03 15:14:08 +0200

    JavaTest002

---
+++ src/com/tourneureric/javalearn005/JavaTest002.java
@@ -0,0 +1,66 @@
+package com.tourneureric.javalearn005;
+
+import java.util.Scanner;
+
+public class JavaTest002 {
+
+	Scanner sc;
+
+	private void doit() {
+
+		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [begin]");
+
+		try {
+			System.out.println(" =>" + (1/0));
+		} catch (ClassCastException e) {
+			e.printStackTrace();
+		}
+		finally{
+			System.out.println("action faite systématiquement");
+		}
+
+		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [end]");
+
+	}
+
+	public void main() {
+
+		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::main() [begin]");
+
+		if (JavaLearn.JL_RUN) {
+
+			try {
+
+				String again = "n";
+
+				sc = new Scanner(System.in);
+
+				do {
+
+					doit();
+
+					P.print("Voulez-vous continuer (o/n) ?");
+					again = sc.nextLine();
+
+				} while (again.equalsIgnoreCase("o"));
+
+				P.print("Fin");
+				sc.close();
+
+			} catch (Exception e) { Log.error("in main()"); throw e; }
+
+			Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::main() [end]");
+
+		}
+
+	}
+
+	public JavaTest002 () {
+
+		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::" + this.getClass().getSimpleName() + "[begin]");
+
+		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::" + this.getClass().getSimpleName() + "[end]");
+
+	}
+
+}
