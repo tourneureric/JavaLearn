64825cb936a1b5974d1cae4d8a10023a9359d7eb
tourneureric <tourneur.eric@gmail.com>
2019-10-03 14:14:32 +0200

    Create package com.tourneureric.javalearn005

---
+++ src/com/tourneureric/javalearn005/JavaTest001.java
@@ -0,0 +1,57 @@
+package com.tourneureric.javalearn005;
+
+import java.util.Scanner;
+
+public class JavaTest001 {
+
+	Scanner sc;
+
+	private void doit() {
+
+		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [begin]");
+
+		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [end]");
+
+	}
+
+	public void main() {
+
+		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::main() [begin]");
+
+		if (JavaLearn.JL_RUN) {
+
+			try {
+
+				String again = "n";
+
+				sc = new Scanner(System.in);
+
+				do {
+
+					doit();
+
+					P.print("Voulez-vous continuer (o/n) ?");
+					again = sc.nextLine();
+
+				} while (again.equalsIgnoreCase("o"));
+
+				P.print("Fin");
+				sc.close();
+
+			} catch (Exception e) { Log.error("in main()"); throw e; }
+
+			Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::main() [end]");
+
+		}
+
+	}
+
+	public JavaTest001 () {
+
+		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::" + this.getClass().getSimpleName() + "[begin]");
+
+		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::" + this.getClass().getSimpleName() + "[end]");
+
+	}
+
+}
