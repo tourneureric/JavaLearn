8ef593823d9dabab8a280896a11da54d9c4b5a83
tourneureric <tourneur.eric@gmail.com>
2019-10-14 18:05:54 +0200

    Créez des collections d'objets

--- src/com/tourneureric/javalearn006/JavaTest001.java
+++ src/com/tourneureric/javalearn006/JavaTest001.java
@@ -1,5 +1,13 @@
 package com.tourneureric.javalearn006;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class JavaTest001 {
@@ -9,6 +17,63 @@ public class JavaTest001 {
 	private void doit() {
 
 		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [begin]");
+
+		List l = new LinkedList();
+		l.add(12);
+		l.add("toto ! !");
+		l.add(12.20f);
+
+		Log.log(Log.LOG_TRACE, "l.size() = " + l.size());
+
+		for (int i = 0; i < l.size(); i++) {
+			P.print("Élément à l'index " + i + " = " + l.get(i));
+		}
+
+		P.print("\n \tParcours avec un itérateur ");
+		P.print("-----------------------------------");
+
+		ListIterator li = l.listIterator();
+		while (li.hasNext()) {
+			P.print(li.next());
+		}
+
+		ArrayList al = new ArrayList();
+		al.add(12);
+		al.add("Une chaîne de caractères !");
+		al.add(12.20f);
+		al.add('d');
+
+		for(int i = 0; i < al.size(); i++)
+		{
+			P.print("donnée à l'indice " + i + " = " + al.get(i));
+		}
+
+		Hashtable ht = new Hashtable();
+		ht.put(1, "printemps");
+		ht.put(10, "été");
+		ht.put(12, "automne");
+		ht.put(45, "hiver");
+
+		Enumeration e = ht.elements();
+
+		while(e.hasMoreElements())
+			P.print(e.nextElement());
+				
+	    HashSet hs = new HashSet();
+	    hs.add("toto");
+	    hs.add(12);
+	    hs.add('d');
+	    
+	    Iterator it = hs.iterator();
+	    while(it.hasNext())
+	      System.out.println(it.next());	    
+
+	    System.out.println("\nParcours avec un tableau d'objet");
+	    System.out.println("-----------------------------------");
+	                
+	    Object[] obj = hs.toArray();
+	    for (Object o : obj)
+	      System.out.println(o); 
 
 		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [end]");
 
