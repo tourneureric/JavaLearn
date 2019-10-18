e7b811d05b0c514e1a3913b72c514bb09ad46ece
tourneureric <tourneur.eric@gmail.com>
2019-10-18 14:43:35 +0200

    Add class JavaTest002

--- src/com/tourneureric/javalearn007/JavaLearn.java
+++ src/com/tourneureric/javalearn007/JavaLearn.java
@@ -5,7 +5,8 @@ public class JavaLearn {
 	public static final boolean JL_RUN = true;
 
 	private static boolean onJavaTestTemplate = false;
-	private static boolean onJavaTest001 = true;
+	private static boolean onJavaTest001 = false;
+	private static boolean onJavaTest002 = true;
 
 	public static void main(String[] args) {
 
@@ -13,6 +14,7 @@ public class JavaLearn {
 
 		if (onJavaTestTemplate) { JavaTestTemplate javaTest = new JavaTestTemplate(); javaTest.main(); }
 		if (onJavaTest001) { JavaTest001 javaTest = new JavaTest001(); javaTest.main(); }
+		if (onJavaTest002) { JavaTest002 javaTest = new JavaTest002(); javaTest.main(); }
 
 		Log.log(Log.LOG_DEBUG, "JavaLearn::main() [end]");
 
