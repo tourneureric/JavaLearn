57b3a0dd1384686ce58c3604cfd061dfb64b637d
tourneureric <tourneur.eric@gmail.com>
2019-10-18 15:15:14 +0200

    play with objects FileInputStream and FileOutputStream

--- src/com/tourneureric/javalearn007/JavaTest002.java
+++ src/com/tourneureric/javalearn007/JavaTest002.java
@@ -1,6 +1,11 @@
 package com.tourneureric.javalearn007;
 
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream; 
+import java.io.IOException;
 
 public class JavaTest002 {
 
@@ -9,6 +14,48 @@ public class JavaTest002 {
 	private void doit() {
 
 		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [begin]");
+		
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		
+		try {
+			fis = new FileInputStream(new File("test.txt"));
+			fos = new FileOutputStream(new File("test2.txt"));
+			
+			byte[] buf = new byte[8];
+			int n = 0;
+			
+			while ((n = fis.read(buf)) >= 0) {
+				fos.write(buf);
+				
+				for (byte bit : buf) {
+					P.print("\t" + bit + "(" + (char) bit + ")");
+				}
+				P.println("");
+				buf = new byte[8];
+			}
+			P.print("Copie terminée !");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fis != null) {
+					fis.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			try {
+				if (fos != null) {
+					fos.close();
+				}				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
 		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [end]");
 
@@ -30,12 +77,12 @@ public class JavaTest002 {
 
 					doit();
 
-					P.print("Voulez-vous continuer (o/n) ?");
+					P.println("Voulez-vous continuer (o/n) ?");
 					again = sc.nextLine();
 
 				} while (again.equalsIgnoreCase("o"));
 
-				P.print("Fin");
+				P.println("Fin");
 				sc.close();
 
 			} catch (Exception e) { Log.error("in main()"); throw e; }
