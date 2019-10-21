1f277eaacc1cfd462ced2662aed98e5d09649a82
tourneureric <tourneur.eric@gmail.com>
2019-10-21 10:58:58 +0200

    Play with Serializable

--- src/com/tourneureric/javalearn007/JavaTest006.java
+++ src/com/tourneureric/javalearn007/JavaTest006.java
@@ -2,6 +2,18 @@ package com.tourneureric.javalearn007;
 
 import java.util.Scanner;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class JavaTest006 {
 
 	Scanner sc;
@@ -9,6 +21,44 @@ public class JavaTest006 {
 	private void doit() {
 
 		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [begin]");
+
+		ObjectInputStream ois;
+		ObjectOutputStream oos;
+
+		try {
+
+			oos = new ObjectOutputStream(
+					new BufferedOutputStream(
+							new FileOutputStream(
+									new File("game.txt"))));
+
+			oos.writeObject(new Game("Assassin Creed", "Aventure", 45.69));
+			oos.writeObject(new Game("Tomb Raider", "Plateforme", 23.45));
+			oos.writeObject(new Game("Tetris", "Stratégie", 2.50));
+			oos.close();
+
+			ois = new ObjectInputStream(
+					new BufferedInputStream(
+							new FileInputStream(
+									new File("game.txt"))));
+
+			try {
+				System.out.println("Affichage des jeux :");
+				System.out.println("*************************\n");
+				System.out.println(((Game)ois.readObject()).toString());
+				System.out.println(((Game)ois.readObject()).toString());
+				System.out.println(((Game)ois.readObject()).toString());
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			ois.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [end]");
 
