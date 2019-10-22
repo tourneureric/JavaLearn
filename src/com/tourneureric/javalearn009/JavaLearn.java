8ce6540e9684236aa677b5dc6f168c9a2fbbcd9a
tourneureric <tourneur.eric@gmail.com>
2019-10-22 12:52:34 +0200

    Create package com.tourneureric.javalearn009

---
+++ src/com/tourneureric/javalearn009/JavaLearn.java
@@ -0,0 +1,21 @@
+package com.tourneureric.javalearn009;
+
+public class JavaLearn {
+
+	public static final boolean JL_RUN = true;
+
+	private static boolean onJavaTestTemplate = false;
+	private static boolean onJavaTest001 = true;
+
+	public static void main(String[] args) {
+
+		Log.log(Log.LOG_DEBUG, "JavaLearn::main() [begin]");
+
+		if (onJavaTestTemplate) { JavaTestTemplate javaTest = new JavaTestTemplate(); javaTest.main(); }
+		if (onJavaTest001) { JavaTest001 javaTest = new JavaTest001(); javaTest.main(); }
+
+		Log.log(Log.LOG_DEBUG, "JavaLearn::main() [end]");
+
+	}
+
+}
---
+++ src/com/tourneureric/javalearn009/P.java
@@ -0,0 +1,19 @@
+package com.tourneureric.javalearn009;
+
+public class P {
+
+	private static final boolean PRINT_ON = true;
+
+	public static final void print(Object pObject) {
+		if (PRINT_ON) {
+			System.out.print(pObject);
+		}
+	}
+	
+	public static final void println(Object pObject) {
+		if (PRINT_ON) {
+			System.out.println(pObject);
+		}
+	}
+
+}
