4f60d82eb42053ed9e427eebec1512882e1a8e30
tourneureric <tourneur.eric@gmail.com>
2019-10-22 12:04:18 +0200

    P.print & P.println

--- src/com/tourneureric/javalearntemplate/P.java
+++ src/com/tourneureric/javalearntemplate/P.java
@@ -6,6 +6,12 @@ public class P {
 
 	public static final void print(Object pObject) {
 		if (PRINT_ON) {
+			System.out.print(pObject);
+		}
+	}
+	
+	public static final void println(Object pObject) {
+		if (PRINT_ON) {
 			System.out.println(pObject);
 		}
 	}
