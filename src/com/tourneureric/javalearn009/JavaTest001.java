75071c39ef7874f0bd5f0eccfd0df45609c4b367
tourneureric <tourneur.eric@gmail.com>
2019-10-24 13:49:50 +0200

    Utilisez les streams

--- src/com/tourneureric/javalearn009/JavaTest001.java
+++ src/com/tourneureric/javalearn009/JavaTest001.java
@@ -1,6 +1,9 @@
 package com.tourneureric.javalearn009;
 
 import java.util.Scanner;
+import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.List;
 
 public class JavaTest001 {
 
@@ -9,6 +12,28 @@ public class JavaTest001 {
 	private void doit() {
 
 		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [begin]");
+
+		List<Personne> listP = Arrays.asList(
+				new Personne(1.80, 70, "A", "Nicolas", Couleur.BLEU),
+				new Personne(1.56, 50, "B", "Nicole", Couleur.VERRON),
+				new Personne(1.75, 65, "C", "Germain", Couleur.VERT),
+				new Personne(1.68, 50, "D", "Michel", Couleur.ROUGE),
+				new Personne(1.96, 65, "E", "Cyrille", Couleur.BLEU),
+				new Personne(2.10, 120, "F", "Denis", Couleur.ROUGE),
+				new Personne(1.90, 90, "G", "Olivier", Couleur.VERRON)
+				);
+
+		P.printline();
+		
+		Stream<Personne> sp = listP.stream();
+		sp.forEach(P::println);
+		
+		P.printline();
+		
+		Stream.iterate(1, (x) -> x + 1).limit(10).forEach(System.out::println);
+		
+		P.printline();
+
 
 		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [end]");
 
