84733bf19d9e012fe1cf25040b0c4338819d0cf7
tourneureric <tourneur.eric@gmail.com>
2019-10-25 14:12:53 +0200

    Gestion du temps humain

--- src/com/tourneureric/javalearn009/JavaTest002.java
+++ src/com/tourneureric/javalearn009/JavaTest002.java
@@ -1,6 +1,11 @@
 package com.tourneureric.javalearn009;
 
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
 
 public class JavaTest002 {
 
@@ -9,6 +14,43 @@ public class JavaTest002 {
 	private void doit() {
 
 		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [begin]");
+		
+		P.printline();
+		
+		LocalDateTime currentTime = LocalDateTime.now();
+		P.println("Date et heure courante : " + currentTime);
+		
+		LocalDate date1 = currentTime.toLocalDate();
+		P.println("Date courante : " + date1);
+		
+		Month mois = currentTime.getMonth();
+		int jour = currentTime.getDayOfMonth();
+		int heure = currentTime.getHour();
+		
+		P.println("Mois : " + mois + ", jour : " + jour + ", heure : " + heure);
+		
+		LocalDateTime date2 = currentTime.withDayOfMonth(25).withYear(2023).withMonth(12);
+		P.println("Date modifiée : " + date2);
+		
+		LocalDate date3  = LocalDate.of(2023, Month.DECEMBER, 25);
+		P.println("Autre façon de fauire : " + date3);
+		
+		LocalTime parsed = LocalTime.parse("20:15:30");
+		P.println("Date parsée : " + parsed);
+		
+		P.printline();
+		
+		LocalDateTime ldt = LocalDateTime.of(2018, Month.DECEMBER, 25, 13, 17, 0);
+		P.println("Date de référence : " + ldt);
+		
+		P.println("+1 semaine : " + ldt.plus(1, ChronoUnit.WEEKS));
+		P.println("+2 mois : " + ldt.plus(2, ChronoUnit.MONTHS));
+		P.println("+3 ans : " + ldt.plus(3, ChronoUnit.YEARS));
+		P.println("+4 heures : " + ldt.plus(4, ChronoUnit.HOURS));
+		P.println("-5 secondes : " + ldt.minus(5, ChronoUnit.SECONDS));
+		P.println("-38 minutes : " + ldt.minusMinutes(38));
+		
+		P.printline();
 
 		Log.log(Log.LOG_DEBUG, this.getClass().getSimpleName() + "::doit() [end]");
 
